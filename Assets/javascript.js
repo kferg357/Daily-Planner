00cb08367bc55bcb43d0be2c2a9152c81c44d59e
kferg357 <kennethferguson90@gmail.com>
2021-04-29 17:39:02 -0400

    modified assets

--- Assets/javascript.js
+++ Assets/javascript.js
@@ -10,3 +10,26 @@ function displayTime() {
   }
 
   setInterval(displayTime, 1000);
+
+
+  //
+  $(document).ready(function () {
+    // var timeInputs = JSON.parse(localStorage.getItem('timeInputs')) || {};
+    var currentHour = moment().hours();
+    var todaysDate = moment().format("dddd, MMMM Do YYYY");
+    console.log(currentHour);
+    //Create Variable with the hours.
+    var timeInputs = [
+        { time: 7, input: "Wake up" },
+        { time: 8, input: "" },
+        { time: 9, input: ""  },
+        { time: 10, input: "" },
+        { time: 11, input: "Go to work" },
+        { time: 12, input: "" },
+        { time: 1, input: "" },
+        { time: 2, input: "" },
+        { time: 3, input: "" },
+        { time: 4, input: "" },
+        { time: 5, input: "" },
+       
+   
